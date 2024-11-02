734a4c9ed814da89398297e5dc349320bbb857d0
JohnDev <johndarwinlambon@gmail.com>
2024-11-02 19:27:31 +0800

    Update

--- frontend/src/layouts/empty/BlankLayout.vue
+++ frontend/src/layouts/empty/BlankLayout.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- src/layouts/BlankLayout.vue -->
 <template>
     <div>
@@ -13,19 +12,3 @@
   <style scoped>
   /* Add layout-specific styles here */
   </style>
-=======
-<!-- src/layouts/empty/BlankLayout.vue -->
-<template>
-  <div>
-    <!-- Any global wrapper elements you want -->
-    <router-view /> <!-- This is where the child components (Login, Register) will be injected -->
-  </div>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-/* Add layout-specific styles here */
-</style>
->>>>>>> origin/Kimberly
--- frontend/src/router/AuthRoutes.js
+++ frontend/src/router/AuthRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import DefaultLayout from '@/layouts/DefaultLayout.vue';
 // import AuthLayout from '@/layouts/AuthLayout.vue';
 
@@ -9,38 +8,19 @@ const AuthRoutes = {
   component: () => import('@/layouts/empty/BlankLayout.vue'),
   meta: {
     requiresAuth: false
-=======
-const AuthRoutes = {
-  path: '/account',
-  component: () => import('@/layouts/empty/BlankLayout.vue'), // Use BlankLayout for wrapping auth pages
-  meta: {
-    requiresAuth: false // These routes do not require authentication
->>>>>>> origin/Kimberly
   },
   children: [
     {
       name: 'Login',
       path: 'login',
-<<<<<<< HEAD
       component: () => import('@/views/auth/Login.vue')
-=======
-      component: () => import('@/views/auth/Login.vue') // Renders the Login component inside BlankLayout
->>>>>>> origin/Kimberly
     },
     {
       name: 'Register',
       path: 'register',
-<<<<<<< HEAD
       component: () => import('@/views/auth/Register.vue')
-=======
-      component: () => import('@/views/auth/Register.vue') // Renders the Register component inside BlankLayout
->>>>>>> origin/Kimberly
     }
   ]
 };
 
-<<<<<<< HEAD
 export default AuthRoutes;
-=======
-export default AuthRoutes;
->>>>>>> origin/Kimberly
